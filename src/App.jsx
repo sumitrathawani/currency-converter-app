b21ceeab707302ae2bbd45e7d07ef6bb1260ebbb
sumitrathawani1810 <sumitrathawani899@gmail.com>
2024-04-12 22:32:27 +0530

    add todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import CurrencyConvertor from "./components/currency-convertor";
 
+// TODO: add proper types and interface later
 const App = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
